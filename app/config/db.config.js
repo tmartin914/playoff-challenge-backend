6660e1fc033a13f91463a3721adbb2ac857cf5ae
Tyler Martin <tmartin024@gmail.com>
2024-01-09 17:15:53 -0500

    setup heroku db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "playoff_challenge",
+  HOST: "us-cluster-east-01.k8s.cleardb.net",
+  USER: "bb30c340a1b0b3",
+  PASSWORD: "c2fdfec8",
+  DB: "heroku_f6a0378c464d1ae",
   dialect: "mysql",
   pool: {
     max: 5,
